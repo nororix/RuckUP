64030521b46ef914f80cbfbf0237f3f3784f94ce
=Noelia <=>
2025-06-18 21:28:16 +0200

    temporal

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,20 @@ require ('dotenv').config();
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  process.env.FRONTEND_URL
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   process.env.FRONTEND_URL
+// ];
 
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true
+// }));
 app.use(cors({
-  origin: allowedOrigins,
+  origin: '*',
   credentials: true
 }));
+
 
 app.use(express.json());
 
