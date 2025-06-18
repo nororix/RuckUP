97243067ae049dc6c4eb1bd0732295c1740b8375
=Noelia <=>
2025-06-18 20:33:11 +0200

    deploy frontend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 const axiosClient = axios.create({
-    baseURL:'http://localhost:3000/api',
+  baseURL: baseURL + '/api',
 });
 
 axiosClient.interceptors.request.use((config)=> {
